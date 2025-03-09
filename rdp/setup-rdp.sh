5e8e09e703c4291f5a654b14e737a95a8ea42cec
Ubuntu <ubuntu@ip-10-0-2-126.ap-northeast-1.compute.internal>
2025-03-09 10:04:13 +0000

    rdp script

---
+++ rdp/setup-rdp.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Update and upgrade system packages
+sudo apt update && sudo apt upgrade -y
+
+# Install Ubuntu desktop environment
+sudo apt install -y ubuntu-desktop
+
+# Install and configure xRDP for remote desktop access
+sudo apt install -y xrdp
+sudo systemctl enable --now xrdp
+
+# Set a password for the user (modify as needed)
+USERNAME="ubuntu"
+PASSWORD="testpassword2025"
+echo "$USERNAME:$PASSWORD" | sudo chpasswd
+
+# Confirm xRDP status
+sudo systemctl status xrdp --no-pager
+
+
+
+# https://docs.docker.com/engine/install/ubuntu/
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
